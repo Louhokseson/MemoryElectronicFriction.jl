24b808e9ef8d3d5260c76789809366fe024d308b
Louhokseson <88427189+Louhokseson@users.noreply.github.com>
2025-07-09 12:01:04 +0100

    au updates

--- scripts/plot/plot_brandyge_dos.jl
+++ scripts/plot/plot_brandyge_dos.jl
@@ -11,27 +11,38 @@ using CairoMakie
 using HokseonPlots
 using ColorSchemes
 using Colors
+using Unitful, UnitfulAtomic
 colorscheme = ColorScheme(parse.(Colorant, ["#045275", "#089099", "#7CCBA2", "#FCDE9C", "#F0746E", "#DC3977", "#7C1D6F"]));
 colormap = HokseonPlots.NICECOLORS;
 
 
-function plot_brandbyge_dos(r = 1.0)
+function plot_brandbyge_dos(r = austrip(1.0*u"Å"))
     m = AndersonImpurityModels.BrandbygeAdsorbate()
     lorentzian = HokseonReproduce.DOS(r, m)
 
     # evaluate the DOS
-    ω = range(0, 6, length=1000)
-    dos = HokseonReproduce.PDF.(ω, lorentzian)
+
+
+    ω = range(-5, 5, length=1000)
+
+    ω_au = austrip.(ω * u"eV") # Convert to atomic units
+    dos_au = HokseonReproduce.PDF.(ω_au, lorentzian)
+
+    dos = ustrip.(auconvert.(u"eV^-1",dos_au)) # Convert to eV
+
+
 
     ## Plotting set up
     fig = Figure(size=(HokseonPlots.RESOLUTION[1]*2, 3*HokseonPlots.RESOLUTION[2]), figure_padding=(1, 2, 1, 1), fonts=(;regular=projectdir("fonts", "MinionPro-Capt.otf")))
-    ax = MyAxis(fig[1,1], xlabel="Energy / eV", ylabel= "Density of States",limits=(nothing, nothing, nothing, nothing))
+    ax = MyAxis(fig[1,1], xlabel="Energy / eV", ylabel= "Density of States / eV⁻¹",limits=(nothing, nothing, nothing, nothing))
 
 
-    lines!(ax, ω, dos; color = colormap[1], linewidth = 2, label = "Brandbyge \n impurity at $r")
+    lines!(ax, ω, dos; color = colormap[1], linewidth = 2, label = "Brandbyge \n impurity at $(auconvert(u"Å",r))")
     band!(ax, ω, zeros(length(ω)), dos; color=(colormap[1],0.3))
     Legend(fig[1,1], ax, tellwidth=false, tellheight=false, valign=:top, halign=:left, margin=(5, 5, 5, 5), orientation=:vertical)
     return fig
 end
 
-plot_brandbyge_dos(1.0)
+r = austrip(0.5*u"Å")
+
+plot_brandbyge_dos(r)
